a519e93eb462b7dbd7842e9aed31784b257ef27b
mrpiyushprajapati <mrpiyushprajapati@gmail.com>
2021-10-22 03:24:08 +0530

    added contacts

---
+++ contacts.jsx
@@ -0,0 +1,30 @@
+const contacts = [
+    {
+      id: 1,
+      name: "Piyush",
+      imgURL:
+        "https://picsum.photos/200",
+      phone: "+91 3234442729",
+      email: "piyush@dummmy.com"
+    },
+    { 
+      id: 2,
+      name: "Dummy",
+      imgURL:
+        "https://picsum.photos/200",
+      phone: "+91 324623323",
+      email: "dummy@hails.com"
+    },
+    {
+      id: 3,
+      name: "P Pupet",
+      imgURL:
+        "https://picsum.photos/200",
+      phone: "+91 986312574",
+      email: "pupel@mail.com"
+    }
+  ];
+
+
+export default contacts;
+  
